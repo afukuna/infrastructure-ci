f5977439e8bc375128d08fccae8997b8afd03009
root <root@localhost.localdomain>
2017-05-08 22:47:25 +0900

    update test2.js

--- identitest/test2.js
+++ identitest/test2.js
@@ -1,11 +1,23 @@
-var webdriver = require('selenium-webdriver'),
-By = webdriver.By,
-until = webdriver.until;
+var webdriver = require('selenium-webdriver');
+var t = require('selenium-webdriver/testing');
+var driver;
+var assert = require('assert');
+var By = webdriver.By;
 
-var driver = new webdriver.Builder()
-.forBrowser('firefox')
-.usingServer('http://localhost:4444/wd/hub')
-.build();
+t.describe('テスト', function () {
+  t.before(function () {
+    driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub')
+      .withCapabilities(webdriver.Capabilities.firefox()).build();
+  });
 
-driver.get('http://www.google.com/');
-driver.quit();
+  t.after(function () {
+    driver.quit();
+  });
+
+  t.it('check return 200', function (done) {
+    driver.get('http://www.google.co.jp').then(function (res) {
+      assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+});
