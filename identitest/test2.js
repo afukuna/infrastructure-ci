650f06eed3bf1945a521b308e6ee0f2f76feb525
root <root@localhost.localdomain>
2017-05-09 00:17:03 +0900

    edit test2.js

--- identitest/test2.js
+++ identitest/test2.js
@@ -5,20 +5,24 @@ var assert = require('assert');
 var By = webdriver.By;
 
 t.describe('テスト', function () {
-  t.before(function () {
-    driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub')
-      .withCapabilities(webdriver.Capabilities.firefox()).build();
-  });
+//  t.before(function () {
+//    driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub')
+//      .withCapabilities(webdriver.Capabilities.firefox()).build();
+//  });
 
-  t.after(function () {
-    driver.quit();
-  });
+//  t.after(function () {
+//    driver.quit();
+//  });
 
   t.it('check return 200', function () {
+    driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub')
+      .withCapabilities(webdriver.Capabilities.firefox()).build();
     driver.get('http://www.google.co.jp').then(function (res) {
       assert.equal(200, res.statusCode);
+      driver.quit();
     }).catch(function (e) { // catch handlerを追加
-      done(e)
+      done(e);
+      driver.quit();
     });
   });
 });
