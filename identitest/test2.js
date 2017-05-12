5f7538962a3df71a73db0d750e3f842b4eac80e8
root <root@localhost.localdomain>
2017-05-13 01:01:37 +0900

    edit test2.js

--- identitest/test2.js
+++ identitest/test2.js
@@ -1,29 +1,17 @@
-var webdriver = require('selenium-webdriver');
-var t = require('selenium-webdriver/testing');
-var driver;
-var assert = require('assert');
-var By = webdriver.By;
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By;
 
-t.describe('テスト', function () {
-//  t.before(function () {
-//    driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub')
-//      .withCapabilities(webdriver.Capabilities.firefox()).build();
-//  });
+describe('tedt', function () {
 
-//  t.after(function () {
-//    driver.quit();
-//  });
+    it('tedt', function (done) {
 
-  t.it('check return 200', function (done) {
-    driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub')
-      .withCapabilities(webdriver.Capabilities.firefox()).build();
-    driver.get('http://www.google.co.jp').then(function (res) {
-      assert.equal(200, res.statusCode);
-      done();
-      driver.quit();
-    }).catch(function (e) { // catch handlerを追加
-      done();
-      driver.quit();
+        var driver = new webdriver.Builder()
+            .forBrowser('firefox')
+            .usingServer('http://localhost:4444/wd/hub')
+            .build();
+
+        driver.get('http://www.google.co.jp')
+        done();
+        driver.quit();
     });
-  });
 });
