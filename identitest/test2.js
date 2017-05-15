2d1f5d69fb84ee9a6dcdfa965c0478f636156a5f
root <root@localhost.localdomain>
2017-05-15 22:48:55 +0900

    edit screenshot

--- identitest/test2.js
+++ identitest/test2.js
@@ -7,29 +7,29 @@ describe('test', function () {
 
     it('test1', function (done) {
 
+        webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
+            return driver.takeScreenshot().then(function(filename) {
+                fs.writeFile('/tmp/' + filename, data.replace(/^data:image\/png;base64,/,''), 'base64', function(err) {
+                    if(err) throw err;
+                });
+            });
+        };
+        driver.saveScreenshot('sample.png');
+
         var driver = new webdriver.Builder()
             .forBrowser('firefox')
             .usingServer('http://localhost:4444/wd/hub')
             .build();
 
-        driver.get('http://www.google.com/ncr');
-
         driver.findElement(By.name('q')).sendKeys('webdriver');
         driver.findElement(By.name('btnG')).click();
         driver.wait(until.titleIs('webdriver - Google Search'), 1000)
         .then(function(){
-
-            webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
-                return driver.takeScreenshot().then(function(data) {
-                    fs.writeFile('/tmp/' + filename, data.replace(/^data:image\/png;base64,/,''), 'base64', function(err) {
-                        if(err) throw err;
-                    });
-                });
-            };
             driver.saveScreenshot('sample.png');
             done();
             driver.quit();
         }).catch(function(err){
+            driver.saveScreenshot('sample.png');
             done(err);
             driver.quit();
         });
