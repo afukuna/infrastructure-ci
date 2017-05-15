8ec5bc23f39c813076327c95f50cc5e88c1e7dbe
root <root@localhost.localdomain>
2017-05-15 23:18:16 +0900

    edit screenshot

--- identitest/test2.js
+++ identitest/test2.js
@@ -25,7 +25,7 @@ describe('test', function () {
                     });
                 });
             };
-            driver.saveScreenshot('/tmp/sample.png');
+            driver.saveScreenshot('sample.png');
             done();
             driver.quit();
         }).catch(function(err){
