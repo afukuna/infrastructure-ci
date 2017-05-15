0cb6e99bbc770d3d50acffe589f335d62d03c1fa
root <root@localhost.localdomain>
2017-05-15 22:59:22 +0900

    edit screenshot

--- identitest/test2.js
+++ identitest/test2.js
@@ -7,14 +7,6 @@ describe('test', function () {
 
     it('test1', function (done) {
 
-        webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
-            return driver.takeScreenshot().then(function(data) {
-                fs.writeFile(filename, data.replace(/^data:image\/png;base64,/,''), 'base64', function(err) {
-                    if(err) throw err;
-                });
-            });
-        };
-        driver.saveScreenshot('sample.png');
 
         var driver = new webdriver.Builder()
             .forBrowser('firefox')
@@ -25,11 +17,17 @@ describe('test', function () {
         driver.findElement(By.name('btnG')).click();
         driver.wait(until.titleIs('webdriver - Google Search'), 1000)
         .then(function(){
+            webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
+                return driver.takeScreenshot().then(function(data) {
+                    fs.writeFile(filename, data.replace(/^data:image\/png;base64,/,''), 'base64', function(err) {
+                        if(err) throw err;
+                    });
+                });
+            };
             driver.saveScreenshot('sample.png');
             done();
             driver.quit();
         }).catch(function(err){
-            driver.saveScreenshot('sample.png');
             done(err);
             driver.quit();
         });
