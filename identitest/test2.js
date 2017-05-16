b325891cfeb4f50c460e2044dab3dbf842b0ea46
root <root@localhost.localdomain>
2017-05-16 23:52:05 +0900

    edit

--- identitest/test2.js
+++ identitest/test2.js
@@ -16,18 +16,18 @@ describe('test', function () {
 
         driver.findElement(By.name('q')).sendKeys('webdriver');
         driver.findElement(By.name('btnG')).click();
-        driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+        driver.wait(until.titleIs('webdriver - Google Search'), 3000);
 
-        webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
-            return driver.takeScreenshot().then(function(data) {
-                fs.writeFile(filename, data.replace(/^data:image\/png;base64,/,''), 'base64', function(err) {
-                    if(err) throw err;
-                });
-            });
-        };
-        driver.saveScreenshot('sample.png');
+//        webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
+//            return driver.takeScreenshot().then(function(data) {
+//                fs.writeFile(filename, data.replace(/^data:image\/png;base64,/,''), 'base64', function(err) {
+//                    if(err) throw err;
+//                });
+//            });
+//        };
+//        driver.saveScreenshot('sample.png');
         sleep(2000);
-        done();
         driver.quit();
+        done();
     });
 });
