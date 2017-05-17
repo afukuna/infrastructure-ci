ef4671cf1124beac2e511a83b801d57b5992e6ed
root <root@localhost.localdomain>
2017-05-17 22:50:17 +0900

    edit

--- identitest/test2.js
+++ identitest/test2.js
@@ -3,12 +3,16 @@ var By = webdriver.By;
 var until = webdriver.until;
 var fs = require('fs');
 
-describe('test', function () {
+describe('sample test', function () {
+
+console.log('test1');
 
     var driver = new webdriver.Builder()
         .forBrowser('firefox')
         .usingServer('http://localhost:4444/wd/hub')
         .build();
+
+console.log('test2');
 
     webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
         driver.takeScreenshot().then(function(data) {
@@ -18,17 +22,31 @@ describe('test', function () {
         });
     };
 
-    it('test1', function (done) {
+console.log('test3');
+
+    it('sample1', function (done) {
+
+console.log('test4');
 
         driver.get('http://www.google.com/ncr');
 //        driver.manage().window().setSize(400, 400);
 
+console.log('test5');
+
         driver.findElement(By.name('q')).sendKeys('webdriver');
         driver.findElement(By.name('btnG')).click();
         driver.wait(until.titleIs('webdriver - Google Search'), 3000);
+
+console.log('test6');
+
         driver.saveScreenshot('sample.png');
+
+console.log('test7');
         
         driver.quit();
         done();
+
+console.log('test8');
+
     });
 });
