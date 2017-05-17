39553a69a2a6ee07a3365575bc2bd13e605090ba
root <root@localhost.localdomain>
2017-05-17 22:47:20 +0900

    edit

--- identitest/test2.js
+++ identitest/test2.js
@@ -5,27 +5,30 @@ var fs = require('fs');
 
 describe('test', function () {
 
+    var driver = new webdriver.Builder()
+        .forBrowser('firefox')
+        .usingServer('http://localhost:4444/wd/hub')
+        .build();
+
+    webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
+        driver.takeScreenshot().then(function(data) {
+            fs.writeFile(filename, data.replace(/^data:image\/png;base64,/,''), 'base64', function(err) {
+                if(err) throw err;
+            });
+        });
+    };
+
     it('test1', function (done) {
 
-        var driver = new webdriver.Builder()
-            .forBrowser('firefox')
-            .usingServer('http://localhost:4444/wd/hub')
-            .build();
-
         driver.get('http://www.google.com/ncr');
+//        driver.manage().window().setSize(400, 400);
 
         driver.findElement(By.name('q')).sendKeys('webdriver');
         driver.findElement(By.name('btnG')).click();
         driver.wait(until.titleIs('webdriver - Google Search'), 3000);
-
-//        driver.manage().window().setSize(400, 400);
-
-        driver.takeScreenshot().then(function(data) {
-            fs.writeFile('sample.png', data.replace(/^data:image\/png;base64,/,''), 'base64', function(err) {
-                if(err) throw err;
-            });
-            driver.quit();
-            done();
-        });
+        driver.saveScreenshot('sample.png');
+        
+        driver.quit();
+        done();
     });
 });
