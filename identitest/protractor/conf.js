4ef60689650cfa1291532dab323235d7895f5559
root <root@localhost.localdomain>
2017-05-21 20:29:31 +0900

    edit

--- identitest/protractor/conf.js
+++ identitest/protractor/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test-protractor.js'],
+  specs: ['todo-spec.js'],
   capabilities: {
     browserName: 'firefox'
   },
