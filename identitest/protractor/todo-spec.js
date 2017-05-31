e1d97f19804242fdffcb32c6696e39b6def528e9
root <root@localhost.localdomain>
2017-05-31 22:59:17 +0900

    edit

--- identitest/protractor/todo-spec.js
+++ identitest/protractor/todo-spec.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 describe('angularjs homepage todo list', function() {
   it('should add a todo', function() {
     browser.get('https://angularjs.org');
-    browser.driver.manage().window().maximize();
+    browser.driver.manage().window().setSize(4000, 4000);
+//    browser.driver.manage().window().maximize();
 
     // abstract writing screen shot to a file
     function writeScreenShot(data, filename) {
